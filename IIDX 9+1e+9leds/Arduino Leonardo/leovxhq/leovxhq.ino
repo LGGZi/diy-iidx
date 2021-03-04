4678dc307a4a92eff754ac05cb47be680356e779
wufe8 <wufeo8@gmail.com>
2021-03-05 01:53:54 +0800

    edit key config and TT logic

--- IIDX 9+1e+9leds/Arduino Leonardo/leovxhq/leovxhq.ino
+++ IIDX 9+1e+9leds/Arduino Leonardo/leovxhq/leovxhq.ino
@@ -1,15 +1,187 @@
-#include "iivx_leo.h"
+/* 
+  pro micro version
+  edit by wufe8
+  deleted led
+  move encoder and controller pin
+  edit because my board only have pin: 2-10, 14-16, a0-a3 (16 in tolel)
+*/
+
+/*
+  Keyboard.h
+
+  Copyright (c) 2015, Arduino LLC
+  Original code (pre-library): Copyright (c) 2011, Peter Barrett
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+  Copyright (c) 2015, Arduino LLC
+  Original code (pre-library): Copyright (c) 2011, Peter Barrett
+
+  Permission to use, copy, modify, and/or distribute this software for
+  any purpose with or without fee is hereby granted, provided that the
+  above copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+  WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR
+  BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES
+  OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+  SOFTWARE.
+ */
+
+#include <stdint.h>
+#include <Arduino.h>
+#include "PluggableUSB.h"
+
+#if defined(USBCON)
+
+#define _USING_HID
+
+// HID 'Driver'
+// ------------
+#define HID_GET_REPORT        0x01
+#define HID_GET_IDLE          0x02
+#define HID_GET_PROTOCOL      0x03
+#define HID_SET_REPORT        0x09
+#define HID_SET_IDLE          0x0A
+#define HID_SET_PROTOCOL      0x0B
+
+#define HID_HID_DESCRIPTOR_TYPE         0x21
+#define HID_REPORT_DESCRIPTOR_TYPE      0x22
+#define HID_PHYSICAL_DESCRIPTOR_TYPE    0x23
+
+// HID subclass HID1.11 Page 8 4.2 Subclass
+#define HID_SUBCLASS_NONE 0
+#define HID_SUBCLASS_BOOT_INTERFACE 1
+
+// HID Keyboard/Mouse bios compatible protocols HID1.11 Page 9 4.3 Protocols
+#define HID_PROTOCOL_NONE 0
+#define HID_PROTOCOL_KEYBOARD 1
+#define HID_PROTOCOL_MOUSE 2
+
+// Normal or bios protocol (Keyboard/Mouse) HID1.11 Page 54 7.2.5 Get_Protocol Request
+// "protocol" variable is used for this purpose.
+#define HID_BOOT_PROTOCOL  0
+#define HID_REPORT_PROTOCOL 1
+
+// HID Request Type HID1.11 Page 51 7.2.1 Get_Report Request
+#define HID_REPORT_TYPE_INPUT   1
+#define HID_REPORT_TYPE_OUTPUT  2
+#define HID_REPORT_TYPE_FEATURE 3
+
+typedef struct
+{
+  uint8_t len;      // 9
+  uint8_t dtype;    // 0x21
+  uint8_t addr;
+  uint8_t versionL; // 0x101
+  uint8_t versionH; // 0x101
+  uint8_t country;
+  uint8_t desctype; // 0x22 report
+  uint8_t descLenL;
+  uint8_t descLenH;
+} HIDDescDescriptor;
+
+typedef struct 
+{
+  InterfaceDescriptor hid;
+  HIDDescDescriptor   desc;
+  EndpointDescriptor  in;
+} HIDDescriptor;
+
+class HIDSubDescriptor {
+public:
+  HIDSubDescriptor *next = NULL;
+  HIDSubDescriptor(const void *d, const uint16_t l) : data(d), length(l) { }
+
+  const void* data;
+  const uint16_t length;
+};
+
+class HID_ : public PluggableUSBModule
+{
+public:
+  HID_(void);
+  int begin(void);
+  int SendReport(uint8_t id, const void* data, int len);
+  void AppendDescriptor(HIDSubDescriptor* node);
+
+protected:
+  // Implementation of the PluggableUSBModule
+  int getInterface(uint8_t* interfaceCount);
+  int getDescriptor(USBSetup& setup);
+  bool setup(USBSetup& setup);
+  uint8_t getShortName(char* name);
+
+private:
+  uint8_t epType[1];
+
+  HIDSubDescriptor* rootNode;
+  uint16_t descriptorSize;
+
+  uint8_t protocol;
+  uint8_t idle;
+};
+
+// Replacement for global singleton.
+// This function prevents static-initialization-order-fiasco
+// https://isocpp.org/wiki/faq/ctors#static-init-order-on-first-use
+HID_& HID();
+
+#define D_HIDREPORT(length) { 9, 0x21, 0x01, 0x01, 0, 1, 0x22, lowByte(length), highByte(length) }
+
+#endif // USBCON
+
+//  Low level key report: up to 6 keys and shift, ctrl etc at once
+extern volatile uint16_t iivx_led;
+
+typedef struct iivxReport     // Pretty self explanitory. Simple state to store all the joystick parameters
+{
+  uint16_t  buttons;
+  uint8_t   xAxis;
+  uint8_t   yAxis;
+  uint8_t   zAxis;
+} iivxReport_t;
+
+class iivx_
+{
+public:
+  iivx_();
+  void setState(iivxReport_t *report);
+  inline uint16_t readState(){ return iivx_led; };
+};
+
+extern iivx_ iivx;
+
 iivxReport_t report; 
 
 #define REPORT_DELAY 2000
 // Number of microseconds between HID reports
 // 2000 = 500hz
 
-#define ENC_L_A 0
-#define ENC_L_B 1
-#define ENC_L_B_ADDR 3
+#define ENC_L_A 2
+#define ENC_L_B 21
+//active using ENC_L_A with interrupt, so ENC_L_A only avilable in pin 0, 1, 2, 3, 7
+//#define ENC_L_B_ADDR 3
 #define ENCODER_SENSITIVITY (double) 2.34375
 #define ENCODER_PORT PIND
+//PIND is arduino register (0-7 pins) RO, set each bit from each pin, eg:01001110 (pin 1,2,3,6 = HIGH). using bit move right and bit AND to get single bit if necessary
 // encoder sensitivity = number of positions per rotation (600) / number of positions for HID report (256)
 /*
  * connect encoders
@@ -17,11 +189,14 @@ iivxReport_t report;
  */
 
 int tmp;
-uint8_t buttonCount = 9;
+//uint8_t buttonCount = 9;
+uint8_t buttonCount = 11;
 uint8_t lightMode = 0;
 // 0 = reactive lighting, 1 = HID lighting
-uint8_t ledPins[] = {2,3,4,5,6,7,8,9,10};
-uint8_t buttonPins[] = {11,12,13,18,19,20,21,22,23};
+//uint8_t ledPins[] = {2,3,4,5,6,7,8,9,10};
+//uint8_t buttonPins[] = {11,12,13,18,19,20,21,22,23};
+uint8_t ledPins[] = {0,18,0,19,0,20,0,0,0,0,0}; //key 1357(iidx map)
+uint8_t buttonPins[] = {3,4,5,6,7,8,9,10,16,14,15};
 //uint8_t sysPin = 11;
 uint8_t reactiveLightPin = 21;
 uint8_t hidLightPin = 22;
@@ -44,7 +219,9 @@ int32_t encL=0;
  
 
 void doEncL(){
-  if((ENCODER_PORT >> ENC_L_B_ADDR)&1){
+  //if((ENCODER_PORT >> ENC_L_B_ADDR)&1){
+  // not recommand
+  if (digitalRead(ENC_L_B) != HIGH) {
     encL++;
   } else {
     encL--;
