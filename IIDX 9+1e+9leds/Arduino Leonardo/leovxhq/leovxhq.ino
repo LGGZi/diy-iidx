1fe5e63066bce11a79714e5d59cde7455d5c3f62
LGGZi <128584316+LGGZi@users.noreply.github.com>
2024-03-04 19:59:14 +0800

    Add files via upload
    now the accuracy and the reflection speed of the code should be better.
    it works well on my own controller, at least.
    the button debouncing changed into 1ms constant scanning
    pulling rate has set to 1000hz
    scratch now needs a minimum start speed and minimum start angle, every mode have its own rate.
    light effect is closeable now. the reflection light and hid light has been combined together.

--- IIDX 9+1e+9leds/Arduino Leonardo/leovxhq/leovxhq.ino
+++ IIDX 9+1e+9leds/Arduino Leonardo/leovxhq/leovxhq.ino
@@ -189,16 +189,16 @@ extern iivx_ iivx;
 
 iivxReport_t report; 
 
-#define REPORT_DELAY 2000
+#define REPORT_DELAY 1000
 // Number of microseconds between HID reports
 // 2000 = 500hz
 
-#define ENC_L_A 1
-#define ENC_L_B 2
+#define ENC_L_A 2
+#define ENC_L_B 3
 //active using ENC_L_A with interrupt, so ENC_L_A only avilable in pin 0, 1, 2, 3, 7
 //#define ENC_L_B_ADDR 3
 //#define ENCODER_SENSITIVITY (double) 2.34375
-#define ENCODER_SENSITIVITY (double) 8
+#define ENCODER_SENSITIVITY (double) 2.3
 //The smaller the more sensitive
 #define ENCODER_PORT PIND
 //PIND is arduino register (0-7 pins) RO, set each bit from each pin, eg:01001110 (pin 1,2,3,6 = HIGH). using bit move right and bit AND to get single bit if necessary
@@ -207,23 +207,25 @@ iivxReport_t report;
  * connect encoders
  * TURNTABLE to pin 0 and 1
  */
-
-int tmp;
+int numSTARTENCL=4;
+int startencl=0;
 //uint8_t buttonCount = 9;
-uint8_t buttonCount = 11;
-uint8_t lightMode = 0;
-// 0 = reactive lighting, 1 = HID lighting
+uint8_t buttonCount = 10;
+
 //uint8_t ledPins[] = {2,3,4,5,6,7,8,9,10};
 //uint8_t buttonPins[] = {11,12,13,18,19,20,21,22,23};
-uint8_t ledPins[] = {18,19,20,21,22,23,14,0,0,0,0};
-uint8_t buttonPins[] = {3,4,5,6,7,8,9,10,11,12,13};
+uint8_t ledPins[] = {A0,A1,A2,A3,A4,A5,1,11,12,13};
+uint8_t buttonPins[] = {4,5,6,7,8,9,10,SCK,MISO,MOSI};
 uint8_t muiltCount = 4;
-uint8_t muiltPins[][2] = {{10,3}, {10,4}, {10,5}, {10,6}};
+uint8_t muiltPins[][3] = {{SCK,4,8}, {SCK,5,8}, {SCK,6,8}, {SCK,7,8},{SCK,MISO,MOSI}};
 //uint8_t sysPin = 11;
-uint8_t reactiveLightPin = 21;
-uint8_t hidLightPin = 22;
+uint8_t reactiveLightPin = 11;
+uint8_t hidLightPin = 12;
 uint8_t sysInputPins[] = {13,18,19,20};
 int32_t encL=0;
+uint16_t ledbuf=0;
+uint16_t buttonbuf=0;
+int lighton=1;
 /* current pin layout
  *  pins 18 - 23 = A0 - A5
  *  pins 2 to 10 = LED 1 to 7
@@ -245,23 +247,34 @@ uint8_t stopThreshold = 0;
 void doEncL(){
   //if((ENCODER_PORT >> ENC_L_B_ADDR)&1){
   // not recommand
-  if (digitalRead(ENC_L_B) != HIGH) {
-    encL++;
-  } else {
-    encL--;
+  //Serial.print("+p");
+  //startencl++;
+  //if(startencl>numSTARTENCL){
+    if (digitalRead(ENC_L_B) != HIGH) {
+      //Serial.print("++");
+          encL++;
+    } else {
+      //Serial.print("__");
+          encL--;
+    }
   }
-}
+//}
 
 void doEncLAbsoluteOn(){
   stopFlag = 0;
-  if (digitalRead(ENC_L_B) != HIGH) {
-    encL = 255;
-    report.buttons &= ~((uint16_t)1 << (buttonCount + 1));
-    report.buttons |= (uint16_t)1 << buttonCount;
-  } else {
-    encL = -256;
-    report.buttons &= ~((uint16_t)1 << buttonCount);
-    report.buttons |= (uint16_t)1 << (buttonCount + 1);
+  startencl++;
+  if(startencl>numSTARTENCL){
+    if (digitalRead(ENC_L_B) != HIGH) {
+      encL = 255;
+      
+      report.buttons &= ~((uint16_t)1 << buttonCount);
+      report.buttons |= (uint16_t)1 << (buttonCount + 1);
+    } else {
+      encL = -256;
+      
+      report.buttons &= ~((uint16_t)1 << (buttonCount + 1));
+      report.buttons |= (uint16_t)1 << buttonCount;
+    }
   }
 }
 
@@ -270,6 +283,7 @@ void doEncLAbsoluteOff(){
       stopFlag++;
     } else {
       encL = 0;
+      startencl=0;
       report.buttons &= ~((uint16_t)1 << buttonCount);
       report.buttons &= ~((uint16_t)1 << (buttonCount + 1));
       //report.buttons |= (uint16_t)1 << buttonCount;
@@ -280,9 +294,9 @@ void doEncLAbsoluteOff(){
 void lights(uint16_t lightDesc){
   for(int i=0;i<buttonCount;i++){
      if((lightDesc>>i)&1){
-         digitalWrite(ledPins[i],HIGH);
-     } else {
          digitalWrite(ledPins[i],LOW);
+     } else {
+         digitalWrite(ledPins[i],HIGH);
      }
   }
 }
@@ -291,11 +305,11 @@ void lightsTest(uint16_t number = 7){
   number = number > buttonCount ? buttonCount : number;
   for(int i = 0; i < 3; i++){
     for(int j = 0; j < number; j++){
-        digitalWrite(ledPins[j],HIGH);
+        digitalWrite(ledPins[j],LOW);
     }
     delay(100);
     for(int j = 0; j < number; j++){
-        digitalWrite(ledPins[j],LOW);
+        digitalWrite(ledPins[j],HIGH);
     }
     delay(100);
   }
@@ -314,60 +328,74 @@ void setup() {
   pinMode(ENC_L_B,INPUT_PULLUP);
   //attachInterrupt(digitalPinToInterrupt(ENC_L_A), doEncL, RISING); //default
 
-  if(digitalRead(muiltPins[0][0]) != HIGH && digitalRead(muiltPins[0][1]) != HIGH) // mode 0: turntable output relative x-axis signal
-  {
+  if(digitalRead(muiltPins[0][0]) != HIGH && digitalRead(muiltPins[0][1]) != HIGH ) // mode 0: turntable output relative x-axis signal
+  { 
     lightsTest(1);
     attachInterrupt(digitalPinToInterrupt(ENC_L_A), doEncL, RISING);
+    numSTARTENCL=0;
   }
-  else if(digitalRead(muiltPins[1][0]) != HIGH && digitalRead(muiltPins[1][1]) != HIGH) // mode 1: turntable output absolute x-axis and button signal without threshold time
+  else if(digitalRead(muiltPins[1][0]) != HIGH && digitalRead(muiltPins[1][1]) != HIGH ) // mode 1: turntable output absolute x-axis and button signal without threshold time
   {
     lightsTest(2);
-    stopThreshold = 1; //loop threshold that how keep turntable output x-axis signal, defult is 0 which won't reset x-axis value
+    stopThreshold = 3; //loop threshold that how keep turntable output x-axis signal, defult is 0 which won't reset x-axis value
+    numSTARTENCL=1;
     attachInterrupt(digitalPinToInterrupt(ENC_L_A), doEncLAbsoluteOn, RISING);
   }
-  else if(digitalRead(muiltPins[2][0]) != HIGH && digitalRead(muiltPins[2][1]) != HIGH) // mode 2: turntable output absolute x-axis and button signal with low threshold time
+  else if(digitalRead(muiltPins[2][0]) != HIGH && digitalRead(muiltPins[2][1]) != HIGH ) // mode 2: turntable output absolute x-axis and button signal with low threshold time
   {
     lightsTest(3);
-    stopThreshold = 4; //loop threshold that how keep turntable output x-axis signal, defult is 0 which won't reset x-axis value
+    stopThreshold = 5; //loop threshold that how keep turntable output x-axis signal, defult is 0 which won't reset x-axis value
+    numSTARTENCL=2;
     attachInterrupt(digitalPinToInterrupt(ENC_L_A), doEncLAbsoluteOn, RISING);
   }
-  else if(digitalRead(muiltPins[3][0]) != HIGH && digitalRead(muiltPins[3][1]) != HIGH) // mode 3: turntable output absolute x-axis and button signal with high threshold time
+  else if(digitalRead(muiltPins[3][0]) != HIGH && digitalRead(muiltPins[3][1]) != HIGH ) // mode 3: turntable output absolute x-axis and button signal with high threshold time
   {
     lightsTest(4);
-    stopThreshold = 8; //loop threshold that how keep turntable output x-axis signal, defult is 0 which won't reset x-axis value
+    stopThreshold = 6; //loop threshold that how keep turntable output x-axis signal, defult is 0 which won't reset x-axis value
+    numSTARTENCL=5;
     attachInterrupt(digitalPinToInterrupt(ENC_L_A), doEncLAbsoluteOn, RISING);
   }
   else // defalut: equal to mode 0
   {
-    attachInterrupt(digitalPinToInterrupt(ENC_L_A), doEncL, RISING); // default
+    
+    lightsTest(1);
+    attachInterrupt(digitalPinToInterrupt(ENC_L_A), doEncL, RISING);
+    numSTARTENCL=0;
   }
+  //Serial.begin(9600);
 }
 
 void loop() {
   // Read buttons
   for(int i=0;i<buttonCount;i++){
     if(digitalRead(buttonPins[i])!=HIGH){
-      report.buttons |= (uint16_t)1 << i;
-    } else {
-      delay(1);
-      if(digitalRead(buttonPins[i])!=HIGH){
+      buttonbuf |= (uint16_t)1 << i;
+    }
+  }
+  delay(1);
+  for(int i=0;i<buttonCount;i++){
+    if(digitalRead(buttonPins[i])!=HIGH && (buttonbuf << i)){
         report.buttons |= (uint16_t)1 << i;
       } else {
         report.buttons &= ~((uint16_t)1 << i);
       }
+    
     }
-  }
   if(stopThreshold != 0){
     doEncLAbsoluteOff();
   }
+  //if(digitalRead(ENC_L_A)==0){startencl=0;}
   // Read Encoders
   report.xAxis = (uint8_t)((int32_t)(encL / ENCODER_SENSITIVITY) % 256);
   // Light LEDs
-  if(lightMode == 0){
-    lights(report.buttons);
-  } else {
-    lights(iivx_led);
-  }
+  if(lighton == 1) {ledbuf=report.buttons|iivx_led;}else{ledbuf=0x00;}
+  lights(ledbuf);
+  if(digitalRead(muiltPins[4][0]) != HIGH && digitalRead(muiltPins[4][1]) != HIGH && digitalRead(muiltPins[4][2]) != HIGH && digitalRead(buttonPins[1]) != HIGH )
+    {
+      if(lighton==1){lighton=0;}
+      else{lighton=1;}
+      while(digitalRead(muiltPins[4][0]) != HIGH && digitalRead(muiltPins[4][1]) != HIGH && digitalRead(muiltPins[4][2]) != HIGH && digitalRead(buttonPins[1]) != HIGH){}
+    }
   // Detect Syspin Entries
   //if(digitalRead(buttonPins[0])!=HIGH){
   //  report.buttons = 0;
@@ -378,12 +406,7 @@ void loop() {
    //     report.buttons &= ~((uint16_t)1 << (i+buttonCount));
  //     }
   //  }
- //   if(digitalRead(reactiveLightPin)!=HIGH){
-  //    lightMode = 0;
- //   } else if (digitalRead(hidLightPin)!=HIGH){
- //     lightMode = 1;
- //   }
- // }
+// }
   // Send report and delay
   iivx.setState(&report);
   delayMicroseconds(REPORT_DELAY);
